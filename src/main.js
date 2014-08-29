0f315d1b84ee8324277a24e82aea8ce5990b1a04
Thibaud Colas <thibaudcolas@gmail.com>
2014-08-29 21:42:10 +0200

    Add naive color collision detection

--- src/main.js
+++ src/main.js
@@ -43,6 +43,42 @@ var reset = function () {
   };
 };
 
+var getColorAmount = function (x, y, w, h, color) {
+  var pixels = ctx.getImageData(x, y, w, h);
+  var all = pixels.data.length;
+  var amount = 0;
+  for (var i = 0; i < all; i += 4) {
+    if (pixels.data[i] === color.r && pixels.data[i + 1] === color.g && pixels.data[i + 2] === color.b) {
+      amount++;
+    }
+  }
+  return amount;
+};
+
+var getPointColor = function (x, y) {
+  var pixel = ctx.getImageData(x, y, 1, 1);
+  return {
+    r: pixel.data[0],
+    g: pixel.data[1],
+    b: pixel.data[2],
+    a: pixel.data[3]
+  };
+};
+
+var colorCollision = function () {
+  var playerColor = getPointColor(player.x + player.radius / 2, player.y + player.radius / 2);
+  var playerColorAmount = getColorAmount(player.x - player.radius - 10, player.y - player.radius - 10, player.radius * 2 + 20, player.radius * 2 + 20, playerColor);
+
+  if (playerColorAmount < 15000) {
+    ctx.globalAlpha = 0.8 / (1 + rand.range(0, 50) / 100);
+  }
+  else {
+    ctx.globalAlpha = 1;
+  }
+
+  console.log(playerColorAmount);
+};
+
 reset();
 
 raf.start(function (elapsed) {
@@ -75,10 +111,19 @@ raf.start(function (elapsed) {
   ctx.fill();
   ctx.fillRect(player.x - player.radius, player.y, player.radius, player.radius);
   ctx.fillRect(player.x, player.y, player.radius, player.radius);
+
+
+  colorCollision();
+});
+
+kd.SPACE.down(function () {
+  ctx.globalAlpha = 0.8 / (1 + rand.range(0, 50) / 100);
 });
 
 kd.SPACE.up(function () {
   console.log('SPACE');
+
+  ctx.globalAlpha = 1;
 
   jsfxr.playSound([0,,0.1812,,0.1349,0.4524,,0.2365,,,,,,0.0819,,,,,1,,,,,0.5]);
 

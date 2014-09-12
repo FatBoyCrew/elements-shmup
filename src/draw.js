bf1d3d61832bf1360a879d8ea4fccd8ff77f7846
Thibaud Colas <thibaudcolas@gmail.com>
2014-09-12 23:37:41 +0200

    Extract topY

--- src/draw.js
+++ src/draw.js
@@ -6,19 +6,20 @@ module.exports = function (ctx) {
 
       var leftmost = x - width / 2;
       var rightmost = x + width / 2;
+      var topY = y - 15;
       var middleY = y + 22.5;
       var bottomY = y + 80;
       // Quadratric curves example
       ctx.beginPath();
       ctx.moveTo(x,y);
       // first half
-      ctx.bezierCurveTo(x, y, x - 5, y - 15, x - 25, y - 15);
-      ctx.bezierCurveTo(leftmost, y - 15, leftmost, middleY, leftmost, middleY);
+      ctx.bezierCurveTo(x, y, x - 5, topY, x - 25, topY);
+      ctx.bezierCurveTo(leftmost, topY, leftmost, middleY, leftmost, middleY);
       ctx.bezierCurveTo(leftmost, y + 40, x - 35, y + 62, x, bottomY);
       // secund half
       ctx.bezierCurveTo(x + 35, y + 62, rightmost, y + 40, rightmost, middleY);
-      ctx.bezierCurveTo(rightmost, middleY, rightmost, y - 15, x + 25, y - 15);
-      ctx.bezierCurveTo(x + 10, y - 15, x, y, x, y);
+      ctx.bezierCurveTo(rightmost, middleY, rightmost, topY, x + 25, topY);
+      ctx.bezierCurveTo(x + 10, topY, x, y, x, y);
       ctx.fillStyle = 'red';
       ctx.fill();
     }
